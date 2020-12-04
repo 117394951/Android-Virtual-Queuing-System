d6ddd92182e0debbb9595e6c2fbd234895b76897
becca <117394951@umail.ucc.ie>
2020-12-04 15:39:23 +0000

    Add, Delete and Details Passed By an Intent

--- app/src/main/java/com/app/is4401/sociallysafe/activity/MainActivity.java
+++ app/src/main/java/com/app/is4401/sociallysafe/activity/MainActivity.java
@@ -1,11 +1,10 @@
 package com.app.is4401.sociallysafe.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,7 +12,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.app.is4401.sociallysafe.R;
-import com.app.is4401.sociallysafe.model.User;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -23,12 +21,10 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "***********";
-    EditText etFirstName, etLastName, etEmail, etMobile;
-    Spinner spNumGuests;
-    TextView tvTime,tvFullName;
-    Button btnJoin, btnLeave, btnRetrieve ;
-    DatabaseReference ref, leaveRef, retrieveRef;
-    User user;
+
+    TextView tvFullName, tvEmail, tvMobile, tvNumGuest;
+    Button btnJoin, btnLeave;
+    DatabaseReference ref, leaveRef;
     long maxId = 0;
 
     @Override
@@ -38,20 +34,40 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(MainActivity.this, "Firebase connection Success", Toast.LENGTH_LONG).show();
         Log.d(TAG, "Connection to Database Successful");
 
-        etFirstName = findViewById(R.id.etFirstName);
-        etLastName = findViewById(R.id.etLastName);
-        etEmail = findViewById(R.id.etEmail);
-        etMobile = findViewById(R.id.etMobile);
-        spNumGuests = findViewById(R.id.spNumGuests);
+
         btnJoin = findViewById(R.id.btnJoinQueue);
         btnLeave = findViewById(R.id.btnLeave);
-        // btnRetrieve = findViewById(R.id.btnRetrieve);
+        tvFullName = findViewById(R.id.tvFullName);
+        tvEmail = findViewById(R.id.tvEmail);
+        tvMobile = findViewById(R.id.tvMobile);
+        tvNumGuest = findViewById(R.id.tvNumGuests);
 
-        //insert to database code reference from youtube video: https://www.youtube.com/watch?v=iy6WexahCdY
-        //user = new User();
         //variable to connect to database
         ref = FirebaseDatabase.getInstance().getReference().child("User");
+        addValueEventListener();
+        setBtnJoin();
+        setBtnLeave();
 
+        if(getIntent().hasExtra("FULLNAME") || getIntent().hasExtra("EMAIL") || getIntent().hasExtra("MOBILE") || getIntent().hasExtra("NUMGUESTS") || getIntent().hasExtra("TIME")) {
+
+            String FullName = getIntent().getExtras().getString("FULLNAME");
+            String Email = getIntent().getExtras().getString("EMAIL");
+            String Mobile = getIntent().getExtras().getString("MOBILE");
+            String NumGuest = getIntent().getExtras().getString("NUMGUESTS");
+
+            tvFullName.setText(FullName);
+            tvEmail.setText(Email);
+            tvMobile.setText(Mobile);
+            tvNumGuest.setText(NumGuest);
+        }
+    }
+
+    private void deleteUser(){
+        leaveRef = FirebaseDatabase.getInstance().getReference().child("User").child(String.valueOf(maxId));
+        leaveRef.removeValue();
+    }
+
+    private void addValueEventListener(){
         //https://www.youtube.com/watch?v=r-g2R_COMqo&list=PLjMaHayx2gDG6bxZEoMuILMVv1Cv-6ua6&index=3 reference to database snapshot and maxId code
         ref.addValueEventListener(new ValueEventListener() {
             @Override
@@ -66,27 +82,19 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "msg: " + error);
             }
         });
+    }
 
+    private void setBtnJoin(){
         btnJoin.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-
-                String etFName = etFirstName.getText().toString();
-                String etLName = etLastName.getText().toString();
-                String etEmailAdd = etEmail.getText().toString();
-                String etMobileN = etMobile.getText().toString();
-
-                if (etFName.isEmpty() || etLName.isEmpty() || etEmailAdd.isEmpty() || etMobileN.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please Fill out All Details", Toast.LENGTH_SHORT).show();
-                    Log.d(TAG, "Invalid attempt to submit");
-                } else {
-                    writeNewUser(etFName, etLName, etEmailAdd, etMobileN , spNumGuests.getSelectedItem().toString());
-                    Toast.makeText(MainActivity.this, "data inserted successfully", Toast.LENGTH_LONG).show();
-                    Log.d(TAG, "Data Inserted into the Database");
-                }
+            public void onClick(View e) {
+                startActivity(new Intent(MainActivity.this, UserActivity.class));
+                Log.d(TAG, "UserActivity started");
             }
         });
+    }
 
+    private void setBtnLeave(){
         btnLeave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -94,21 +102,5 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "Data deleted from the database");
             }
         });
-    }
-
-    //reference youtube video : https://www.youtube.com/watch?v=PqCpz5YtzF4
-    private Long getCurrentTimestamp() {
-        return System.currentTimeMillis() / 1000;
-    }
-
-    private void writeNewUser(String FirstName, String LastName, String Email, String Mobile, String NumGuests){
-        Long time = getCurrentTimestamp();
-        User user = new User(FirstName, LastName, Email, Mobile, NumGuests, time);
-        ref.child(String.valueOf(maxId+1)).setValue(user);
-    }
-
-    private void deleteUser(){
-        leaveRef = FirebaseDatabase.getInstance().getReference().child("User").child(String.valueOf(maxId));
-        leaveRef.removeValue();
     }
 }
