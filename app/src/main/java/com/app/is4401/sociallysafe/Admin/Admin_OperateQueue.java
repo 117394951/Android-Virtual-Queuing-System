62093e7db4fbe004cdced34f2d6611ef45470ca5
becca <117394951@umail.ucc.ie>
2021-02-09 21:52:12 +0000

    fragment to fragment

---
+++ app/src/main/java/com/app/is4401/sociallysafe/Admin/Admin_OperateQueue.java
@@ -0,0 +1,18 @@
+package com.app.is4401.sociallysafe.Admin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.app.is4401.sociallysafe.R;
+//Merc_QueueDisplay
+//this is behind manage queue, operate button
+//here is where you can put next customer etc . . . .
+public class Admin_OperateQueue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin__operate_queue);
+    }
+}
